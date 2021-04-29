93832d2803f968d2b0884bc9a63955b85cd283e8
Lucas Bozzo <lucas.bozzo@icloud.com>
2021-04-28 20:51:55 -0500

    Remove auth

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,36 +1,16 @@
-import Logo from '../Logo';
-import { useTheme } from 'next-themes';
-import { useSession } from 'next-auth/client';
-import Image from 'next/image';
-
-function NavbarProfile() {
-  const session = useSession()[0];
-
-  return (
-    <div className="flex flex-row items-center">
-      <div className="relative w-7 h-7 rounded-full overflow-hidden">
-        <Image src={session?.user.image} layout="fill" objectFit="contain" />
-      </div>
-    </div>
-  );
-}
+import Logo from "../Logo";
+import { useTheme } from "next-themes";
 
 export default function Navbar() {
   const { theme, setTheme } = useTheme();
-  const session = useSession()[0];
 
   return (
     <div className="flex flex-row justify-between items-center py-2">
       <div className="px-1">
-        <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+        <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
           <Logo width={23} height={23} />
         </button>
       </div>
-      {session && (
-        <>
-          <NavbarProfile />
-        </>
-      )}
     </div>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,15 @@
-import 'tailwindcss/tailwind.css';
-import Head from 'next/head';
-import { ThemeProvider } from 'next-themes';
-import { Provider } from 'next-auth/client';
+import "tailwindcss/tailwind.css";
+import Head from "next/head";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
-      <Provider session={pageProps.session}>
-        <Head>
-          <title>Cuspide</title>
-          <meta property="og:title" content="Cuspide" />
-        </Head>
-        <Component {...pageProps} />
-      </Provider>
+      <Head>
+        <title>Cuspide</title>
+        <meta property="og:title" content="Cuspide" />
+      </Head>
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
