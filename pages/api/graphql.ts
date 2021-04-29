0740098370c0049313b4220fdf4a8b4bb067bbbd
Lucas Bozzo <lucas.bozzo@icloud.com>
2021-04-28 22:38:14 -0500

    Graphql with Mock

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -38,10 +38,4 @@ const mocks = {
 
 const apolloServer = new ApolloServer({ typeDefs, mocks });
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
 export default apolloServer.createHandler({ path: "/api/graphql" });
