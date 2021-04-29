234ba64a396732e9af84e232020ee2bef256a9c3
Lucas Bozzo <lucas.bozzo@icloud.com>
2021-04-29 17:46:53 -0500

    Nexus Prisma implementation with SQLite

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,42 +1,8 @@
-import { ApolloServer, MockList } from "apollo-server-micro";
-import { gql } from "apollo-server-micro";
-import * as faker from "faker";
+import { ApolloServer } from "apollo-server-micro";
+import { schema } from "../../server/graphql/schema";
+import { context } from "../../server/graphql/context";
 
-export const typeDefs = gql`
-  type Author {
-    id: ID
-    name: String
-    url: String
-  }
-
-  type Quote {
-    id: ID
-    text: String
-    author: Author!
-  }
-
-  type Query {
-    authors: [Author]
-    quote(ID: ID!): Quote!
-    quotes: [Quote]
-    randomQuote: Quote!
-  }
-`;
-
-const mocks = {
-  Author: () => ({
-    name: faker.name.findName(),
-  }),
-  Quote: () => ({
-    text: faker.lorem.lines(2),
-  }),
-  Query: () => ({
-    authors: () => new MockList(faker.random.number(20)),
-    quotes: () => new MockList(faker.random.number(20)),
-  }),
-};
-
-const apolloServer = new ApolloServer({ typeDefs, mocks });
+const apolloServer = new ApolloServer({ schema, context });
 
 export const config = {
   api: {
---
+++ server/graphql/types/Quote.ts
@@ -0,0 +1,48 @@
+import { intArg, nonNull, objectType } from "nexus";
+import { extendType } from "nexus";
+import { Author } from "./Author";
+import { random } from "faker";
+
+export const Quote = objectType({
+  name: "Quote",
+  definition(t) {
+    t.nonNull.int("id");
+    t.nonNull.string("text");
+    t.nonNull.field("author", { type: Author });
+  },
+});
+
+export const QuoteQuery = extendType({
+  type: "Query",
+  definition(t) {
+    t.list.field("quotes", {
+      type: "Quote",
+      resolve(_root, _args, ctx) {
+        return ctx.db.quote.findMany({ include: { author: true } });
+      },
+    });
+    t.field("quote", {
+      type: "Quote",
+      args: {
+        id: nonNull(intArg()),
+      },
+      resolve(_root, args, ctx) {
+        return ctx.db.quote.findUnique({
+          where: {
+            id: args.id,
+          },
+          include: { author: true },
+        });
+      },
+    });
+    t.nonNull.field("randomQuote", {
+      type: "Quote",
+      async resolve(_root, _args, ctx) {
+        return ctx.db.quote.findFirst({
+          skip: random.number((await ctx.db.quote.count()) - 1),
+          include: { author: true },
+        });
+      },
+    });
+  },
+});
